730a298f6c803e735c6fe27862e1a752fd26eabe
codarscoder <codarscoder@gmail.com>
2024-09-05 04:53:09 +0300

    EDT

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Kullanıcının rolünü kontrol ediyoruz
+        if (Auth::check() && Auth::user()->role == 1) {
+            return $next($request); // Rol 1 ise devam et
+        }
+
+        // Rol 1 değilse yetki hatası döndürüyoruz
+        return response()->json(['error' => 'Unauthorized'], 403);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ExamController;
 use App\Http\Controllers\API\QuestionController;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login-start', [AuthController::class, 'loginStart']);
 Route::post('login', [AuthController::class, 'login']);
+
 Route::middleware('auth:api')->group(function () {
-    Route::get('test-token', [AuthController::class, 'testToken']);
+
 
     //exams
     Route::post('/add-exam', [ExamController::class, 'store']);
     Route::put('/update-exam/{id}', [ExamController::class, 'update']);
+    Route::post('/add-question-multiple', [QuestionController::class, 'storeMultiple']);
+
+
+    Route::middleware('isAdmin')->group(function () {
+        Route::get('test-token', [AuthController::class, 'testToken']);
+    });
 });
-Route::post('/add-question-multiple', [QuestionController::class, 'storeMultiple']);
 
