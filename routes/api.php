d5f78bdae8fe9c4428ee81a50a8d017a26954d37
codarscoder <codarscoder@gmail.com>
2024-09-05 04:56:22 +0300

    edt

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,17 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
 
+        Route::get('test-token', [AuthController::class, 'testToken']);
 
+
+
+    Route::middleware('isAdmin')->group(function () {
     //exams
     Route::post('/add-exam', [ExamController::class, 'store']);
     Route::put('/update-exam/{id}', [ExamController::class, 'update']);
     Route::post('/add-question-multiple', [QuestionController::class, 'storeMultiple']);
 
 
-    Route::middleware('isAdmin')->group(function () {
-        Route::get('test-token', [AuthController::class, 'testToken']);
     });
 });
 
